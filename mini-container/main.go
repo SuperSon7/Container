855b01515c047740178847b6c84b8c8b868f2988
VanillaBean <vin990227@naver.com>
2026-05-18 18:38:07 +0900

    feat : Add parent-child container run flow

--- mini-container/main.go
+++ mini-container/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"syscall"
 )
 
@@ -15,15 +17,18 @@ func main() {
 
 	switch os.Args[1] {
 	case "run":
-		run(os.Args[2], os.Args[3:]...)
+		run()
+	case "child":
+		child()
 	default:
 		fmt.Println("unknown command:", os.Args[1])
 	}
 }
 
-func run(command string, args ...string) {
-	cmd := exec.Command(command, args...)
+func run() {
+	fmt.Printf("Running %v \n", os.Args[2:])
 
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -34,8 +39,45 @@ func run(command string, args ...string) {
 			syscall.CLONE_NEWNS, //mount_namespace
 	}
 
-	if err := cmd.Run(); err != nil {
-		fmt.Println("error:", err)
-		os.Exit(1)
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v \n", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(syscall.Sethostname([]byte("container")))
+	must(syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, ""))
+	must(syscall.Chroot("/home/vanillab/container/rootfs")) // must change as your own path
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "/proc", "proc", 0, ""))
+	must(syscall.Mount("tmpfs", "/mytemp", "tmpfs", 0, ""))
+
+	cg()
+
+	must(cmd.Run())
+
+	defer must(syscall.Unmount("proc", 0))
+	defer must(syscall.Unmount("mytemp", 0))
+}
+
+func cg() {
+	cgroups := "/sys/fs/cgroup"
+	pids := filepath.Join(cgroups, "pids")
+	os.MkdirAll(filepath.Join(pids, "jb"), 0755)
+	must(os.WriteFile(filepath.Join(pids, "jb/pids.max"), []byte("20"), 0700))
+	// Removes the new cgroup in place after the container exits
+	must(os.WriteFile(filepath.Join(pids, "jb/notify_on_release"), []byte("1"), 0700))
+	must(os.WriteFile(filepath.Join(pids, "jb/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
